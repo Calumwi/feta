aa0b46335700c280e0e929df6f49e87b69275932
imisiaina <imis.aina1@gmail.com>
2022-11-02 23:49:52 +0000

    Decently Working Signup/Signin Functionality

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -18,6 +18,7 @@ const LogInForm = ({ navigate }) => {
     if(response.status !== 201) {
       console.log("yay")
       navigate('/login')
+      alert("Incorrect Password!")
     } else {
       console.log("oop")
       let data = await response.json()
@@ -34,23 +35,78 @@ const LogInForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
+  const toggleEl = ('click', function() {
+    document.querySelector('.cont').classList.toggle('s--signup');
+  });
+
 
     return (
-      // <div>
-      //   <span class="title">
-      //     Your account has been successfully created
-      //   </span>
-      //   <br/>
-      //   <br/>
-      //   <form onSubmit={handleSubmit}>
-      //   <button id="login-page-btn" type="submit" value="Submit">Log-In</button>
-      //   </form>
-      // </div>
-      <form onSubmit={handleSubmit}>
-        <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-        <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input role='submit-button' id='submit' type="submit" value="Submit" />
-      </form>
+      <div id="container">
+        {/* <div class="topnav">
+          <div class="topnav-centered">
+            <a href="/posts" class="active">Feed</a>
+          </div>
+          <div class="topnav-right">
+            <a href="/login">Login</a>
+            <a href="/logout">Logout</a>
+          </div>
+        </div> */}
+            <div className="cont">
+        <form onSubmit={handleSubmit}>
+          <div className="form sign-in">
+            <h2>Welcome back,</h2>
+            <label>
+              <span>Email</span>
+              <input id="email" type='text' value={ email } onChange={handleEmailChange} />
+            </label>
+            <label>
+              <span>Password</span>
+              <input id="password" type='password' value={ password } onChange={handlePasswordChange} />
+            </label>
+            <p className="forgot-pass">Forgot password?</p>
+            <button id='submit' type="submit" className="submit" value="Submit">Sign In</button>
+          </div>
+        </form>
+      <div className="sub-cont">
+        <div className="img">
+          <div className="img__text m--up">
+            <h2>New here?</h2>
+            <p>Sign up and join the world of cheese lovers!</p>
+          </div>
+          <div className="img__text m--in">
+            <h2>One of us?</h2>
+            <p>If you already has an account, just sign in. We've missed you!</p>
+          </div>
+          <div className="img__btn" onClick={toggleEl}>
+            <span className="m--up">Sign Up</span>
+            <span className="m--in">Sign In</span>
+          </div>
+        </div>
+        
+        <div className="form sign-up">
+          <h2>Time to feel like home,</h2>
+          {/* <label>
+            <span>Name</span>
+            <input type="text" />
+          </label> */}
+          <label>
+            <span>Email</span>
+            <input id="email" type='text' value={ email } onChange={handleEmailChange} />
+          </label>
+          <label>
+            <span>Password</span>
+            <input id="password" type='password' value={ password } onChange={handlePasswordChange} />
+          </label>
+          <button id='submit' type="submit" className="submit" value="Submit">Sign Up</button>
+        </div>
+      </div>
+    </div>
+    </div>
+      // <form onSubmit={handleSubmit}>
+      //   <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
+      //   <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
+      //   <input role='submit-button' id='submit' type="submit" value="Submit" />
+      // </form>
     );
 }
 
